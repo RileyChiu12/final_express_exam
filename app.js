d8ece287087a1098a7290ea67815bc7fdfb6063a
RileyChiu12 <rileychiu12@gmail.com>
2022-05-05 18:17:22 +0800

    update route setting

--- app.js
+++ app.js
@@ -27,17 +27,15 @@ app.use(express.static('public'))
 // app.get('/contact', (req, res) => {
 //   res.render('contact')
 // })
-app.get('/:page',(req,res) =>{
-  const pages = ['about','portfolio','contact']
+app.get('/:page', (req, res) => {
+  const pages = ['about', 'portfolio', 'contact']
   if (pages.includes(req.params.page)) {
     res.render(req.params.page)
-  }else {
+  } else {
     // 找不到的話就回到首頁
     res.render('index')
   }
 })
-
-
 // start  &  listen on Express server
 app.listen(port, () => {
   console.log(`express is listening on localhost:${port} `)
